63eafb1570fdf328ed3738840c5138a4d2a4e138
Patrick Skiebe <patrick@skie.be>
2022-04-12 11:00:44 +0200

    examples: New KDE global Plasma Theme scripts

---
+++ examples/dark-mode.d/kde-global-theme.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Change the global Plasma Theme. On Manjaro you can use "org.manjaro.breath-dark.desktop" 
+# or you can create your own global Plasma Theme with the "Plasma Look And Feel Explorer".
+# Reference: https://userbase.kde.org/Plasma/Create_a_Global_Theme_Package
+
+lookandfeeltool --apply "org.kde.breezedark.desktop"
