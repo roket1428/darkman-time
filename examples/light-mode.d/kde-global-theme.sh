4bd5b47d08fbbaa0761a94a95dfd07fad2275bf1
nachtjasmin <nachtjasmin@posteo.de>
2023-03-26 20:36:18 +0200

    Set -platform offscreen for lookandfeeltool
    Since Plasma 5.26 the current example would crash. A bug is already
    reported [1], along with a fix.
    
    [1]: https://bugs.kde.org/show_bug.cgi?id=460643

--- examples/light-mode.d/kde-global-theme.sh
+++ examples/light-mode.d/kde-global-theme.sh
@@ -3,5 +3,8 @@
 # Change the global Plasma Theme. On Manjaro you can use "org.manjaro.breath-light.desktop" 
 # or you can create your own global Plasma Theme with the "Plasma Look And Feel Explorer".
 # Reference: https://userbase.kde.org/Plasma/Create_a_Global_Theme_Package
+#
+# Since Plasma 5.26 the lookandfeeltool does not work anymore without "faking" the screen.
+# Reference: https://bugs.kde.org/show_bug.cgi?id=460643
 
-lookandfeeltool --apply "org.kde.breeze.desktop"
+lookandfeeltool -platform offscreen --apply "org.kde.breeze.desktop"
