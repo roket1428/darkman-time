29663fbcc8d5110b12ec621dfff8c3eebd9f8213
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-10-12 15:27:33 +0200

    Update usage of deprecated function

--- cmd/darkman/main.go
+++ cmd/darkman/main.go
@@ -21,7 +21,7 @@ var rootCmd = &cobra.Command{
 var setCmd = &cobra.Command{
 	Use:       "set",
 	Short:     "Set the current mode",
-	Args:      cobra.ExactValidArgs(1),
+	Args:      cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	ValidArgs: []string{"dark", "light"},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return libdarkman.SetMode(args[0])
